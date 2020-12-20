19f1847d24cf32666f84a55ead680d3caed9fe7b
George Tang <georgecctang@gmail.com>
2020-12-19 21:42:04 -0500

    complete Day 11 part 2

--- Day11.js
+++ Day11.js
@@ -1,5 +1,21 @@
 const fs = require('fs');
 
+
+const lookForOccupiedSeat = (seatings, rowIndex, colIndex, rowDirection, colDirection) => {
+  let rowPosition = rowIndex + rowDirection;
+  let colPosition = colIndex + colDirection;
+  let seat;
+  while ((rowPosition >= 0 && rowPosition < seatings.length) 
+    && (colPosition >= 0 && colPosition < seatings[0].length)
+    && (!['#', 'L'].includes(seat))
+    ) {
+      seat = seatings[rowPosition][colPosition];
+      rowPosition += rowDirection;
+      colPosition += colDirection;
+  }
+  return seat === "#" ? 1 : 0;
+}
+
 const countOccupiedSeats = (seatings) => {
   let occupiedSeats = 0;
   seatings.forEach(row => {
@@ -36,6 +52,28 @@ const getSurroundingOccupiedSeats = (seatings, rowIndex, colIndex) => {
 
 }
 
+const getSurroundingOccupiedSeats2 = (seatings, rowIndex, colIndex) => {
+  let surroundingOccupiedSeats = 0;
+  // Look top
+  surroundingOccupiedSeats += lookForOccupiedSeat(seatings, rowIndex, colIndex, -1, 0);
+  // Loop top right
+  surroundingOccupiedSeats += lookForOccupiedSeat(seatings, rowIndex, colIndex, -1, 1);
+  // Look right
+  surroundingOccupiedSeats += lookForOccupiedSeat(seatings, rowIndex, colIndex, 0, 1);
+  // Look bottom right
+  surroundingOccupiedSeats += lookForOccupiedSeat(seatings, rowIndex, colIndex, 1, 1);
+  // Look bottom
+  surroundingOccupiedSeats += lookForOccupiedSeat(seatings, rowIndex, colIndex, 1, 0);
+  // Look bottom left
+  surroundingOccupiedSeats += lookForOccupiedSeat(seatings, rowIndex, colIndex, 1, -1);
+  // Look left
+  surroundingOccupiedSeats += lookForOccupiedSeat(seatings, rowIndex, colIndex, 0, -1);
+  // Look top left
+  surroundingOccupiedSeats += lookForOccupiedSeat(seatings, rowIndex, colIndex, -1, -1);
+
+  return surroundingOccupiedSeats;
+}
+
 
 const getNewSeatings = (seatings) => {
 
@@ -68,6 +106,36 @@ const getNewSeatings = (seatings) => {
   return newSeatings;
 }
 
+const getNewSeatings2 = (seatings) => {
+  const newSeatings = [];
+
+  // loop through the existing seatings
+  for (let i = 0; i < seatings.length; i++) {
+    const newSeatingsRow = [];
+    for (let j = 0; j <seatings[0].length; j++) {
+      
+      // set newState to the current state as initial value
+      let newState = seatings[i][j];
+      
+      // evaluate surrounding seats for an existing empty seat
+      if (seatings[i][j] === 'L') {
+        const surroundingOccupiedSeats = getSurroundingOccupiedSeats2(seatings, i, j);
+        if (surroundingOccupiedSeats === 0) {
+          newState = "#";
+        }
+      } else if (seatings[i][j] === '#') {
+        const surroundingOccupiedSeats = getSurroundingOccupiedSeats2(seatings, i, j);
+        if (surroundingOccupiedSeats >= 5) {
+          newState = "L";
+        }
+      }
+      newSeatingsRow.push(newState);
+    }
+    newSeatings.push(newSeatingsRow);
+  }
+  return newSeatings;
+}
+
 const question1 = () => {
   fs.readFile('./data/Day11-data.txt', (err, data) => {
     const seatings = data.toString().split("\n").map(row => row.split(''));
@@ -90,41 +158,31 @@ const question1 = () => {
   })
 };
 
-question1();
+// question1();
+
+const question2 = () => {
+  fs.readFile('./data/Day11-data.txt', (err, data) => {
+    const seatings = data.toString().split("\n").map(row => row.split(''));
+    
+    let newSeatings = getNewSeatings2(seatings);
+    let occupiedSeats = countOccupiedSeats(newSeatings);
+    const occupiedSeatsArray = [occupiedSeats];
+    
+    while (occupiedSeatsArray[occupiedSeatsArray.length - 1] !== occupiedSeatsArray[occupiedSeatsArray.length - 2]) {
+      newSeatings = getNewSeatings2(newSeatings);
+      occupiedSeats = countOccupiedSeats(newSeatings)
+      occupiedSeatsArray.push(occupiedSeats);
+    }
+    
+    console.log(occupiedSeatsArray);
+
+    // const newSeatingsAsStrings = newSeatings.map(row => row.join(''));
+    // newSeatingsAsStrings.forEach(row => console.log(row));
+  
+  })
+};
+
+question2();
 
 
 
-
-
-
-// const ar = [[10,20,30],[40,50,60], [70,80,90]];
-
-// const result = [];
-// for (let i = 0; i < ar.length; i++) {
-//   const resultRow = [];
-//   for (let j = 0; j <ar[0].length; j++){
-//     let sum = 0;
-//     console.log('******');
-//     console.log(ar[i][j]);
-//     for (let row of [i-1, i, i + 1]) {
-//       for (let col of [j-1, j, j + 1]) {
-//         if (row < 0 || row >= ar.length) {
-//           continue;
-//         }
-//         if (col < 0 || col >= ar[0].length) {
-//           continue;
-//         }
-//         if (row === i && col === j) {
-//           continue;
-//         }
-//         console.log('row:', row, 'col:', col, 'number:', ar[row][col]);
-//         sum += ar[row][col];        
-//       }
-//     }
-//     console.log('sum:',sum);
-//     resultRow.push(sum);
-//   }
-//   result.push(resultRow);
-// }
-
-// console.log(result);
